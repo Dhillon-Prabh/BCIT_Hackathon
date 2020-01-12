5edc74e369efc708d5d56605073e4d9f1caae27f
dhillon-Prabh <psingh143105@gmail.com>
2020-01-11 20:55:01 -0800

    backend charts

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const alasql = require('alasql'); //import for alasql
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+app.get('/productivity', (req, res) => {
+  var result = "";
+  alasql.promise('SELECT SHIFT_DATE, PRODUCTIVITY FROM XLSX("data/PRODUCTIVITY-_1_.xlsx")')
+      .then(function(resp){
+          //  console.log(res); // output depends on mydata.xls
+        res.status(200).json(resp);
+      }).catch(function(err){
+           console.log('Does the file exists? there was an error:', err);
+      });
+  // console.log(result);
+  // res.send({ express: 'Hello From Express' });
 });
 
 app.post('/api/world', (req, res) => {
