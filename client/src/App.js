dc50fdf5c66569fc90a68455cf62937eaea97c68
alire <alireza.kakan@gmail.com>
2020-01-12 10:17:07 -0800

    frontend

--- client/src/App.js
+++ client/src/App.js
@@ -27,20 +27,31 @@ handleClick = () => {
 render() {
     return (
       <div className="App">
+        <div className="h-100 bg-info mb-4 p-2">
+          <h3 className="text-light font-weight-bold">Truck Analysis</h3>
+        </div>
 
-        <div className="mixed-chart">
-          {this.state.upload && (
-          <form ref='uploadForm' 
-            id='uploadForm'
-            action='http://localhost:5000/upload-files' 
-            method='post' 
-            encType="multipart/form-data">
-          <input type="file" name="photos" multiple="multiple" className="fileUpload"/>
-          <input type='submit' value='Upload!' className="Button" />
-        </form>)}
+        <div className="mixed-chart mx-auto">
+                {this.state.upload && (
+          <div className="row card border-info shadow-sm w-75 mx-auto p-5">
+            <div className="card-body text-info">
+                  <form ref='uploadForm' 
+                    id='uploadForm' 
+                    action='http://localhost:5000/upload-files' 
+                    method='post' 
+                    encType="multipart/form-data"
+                    className="">
+                    <div className="row">
+                        <input type="file" name="photos" multiple="multiple" className=" fileUpload col-sm-8 btn btn-outline-info border rounded text-white"/>
+                        <input type='submit' value='Upload!' className=" col-sm-3 ml-auto btn btn-outline-success " />
+                    </div>
+                </form>
+            </div>  
+          </div>)}
+          
         
         {this.state.analyze && (
-              <input type="button" value="Analyze!" onClick={this.handleClick}/>
+              <input type="button" className="mt-5 btn btn-warning shadow-sm border rounded-pill w-50 p-3 font-weight-bold" value="Analyze!" onClick={this.handleClick}/>
             )}
         {this.state.charts && (
         <div>
